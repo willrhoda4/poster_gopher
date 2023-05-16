7d7401569d60a3dac2fe78f1ed0ea64d8a87aa90
willrhoda4 <willrhoda4@gmail.com>
2023-05-16 13:55:02 -0500

    vercel deployment4

--- poster_gopher/settings.py
+++ poster_gopher/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = get_random_secret_key()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'poster-gopher.vercel.app'
+]
 
 
 # Application definition
