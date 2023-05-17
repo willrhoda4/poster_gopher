c0ab80d9d779b7bce4bc0747c7f20cd3d98e203e
willrhoda4 <willrhoda4@gmail.com>
2023-05-17 11:13:31 -0500

    adjusted middleware

--- poster_gopher/settings.py
+++ poster_gopher/settings.py
@@ -46,18 +46,18 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
 
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = "poster_gopher.urls"
